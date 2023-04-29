7b47fff5eab3423df70323dceff69a76944cff14
kartikgupta <kartik.gupta2505@gmail.com>
2023-04-29 19:24:45 +0530

    extension react

---
+++ react-chrome-ext/src/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+const root = document.createElement("div")
+root.className = "container"
+document.body.appendChild(root)
+const rootDiv = ReactDOM.createRoot(root);
+rootDiv.render(
+  <React.StrictMode>
+      <App />
+  </React.StrictMode>
+)
+
