e8c8fca66cbb812d96ddf9e77a63db91edbe656d
aviraj5121 <avirajnimja12@gmail.com>
2023-05-01 22:59:26 +0530

    chatgpt added

--- Chatbot extensions/chatbot.js
+++ Chatbot extensions/chatbot.js
@@ -3,9 +3,7 @@ const chatbot = document.getElementById('chatbot');
 const conversation = document.getElementById('conversation');
 const inputForm = document.getElementById('input-form');
 const inputField = document.getElementById('input-field');
-
-// Add event listener to input form
-inputForm.addEventListener('submit', function(event) {
+inputForm.addEventListener('submit', async function(event) {
   // Prevent form submission
   event.preventDefault();
 
@@ -23,7 +21,7 @@ inputForm.addEventListener('submit', function(event) {
   conversation.appendChild(message);
 
   // Generate chatbot response
-  const response = generateResponse(input);
+  const response = await generateResponse(input);
 
   // Add chatbot response to conversation
   message = document.createElement('div');
@@ -34,24 +32,30 @@ inputForm.addEventListener('submit', function(event) {
 });
 
 // Generate chatbot response function
-function generateResponse(input) {
-    // Add chatbot logic here
-    const responses = [
-      "Hello, how can I help you today? 😊",
-      "I'm sorry, I didn't understand your question. Could you please rephrase it? 😕",
-      "I'm here to assist you with any questions or concerns you may have. 📩",
-      "I'm sorry, I'm not able to browse the internet or access external information. Is there anything else I can help with? 💻",
-      "What would you like to know? 🤔",
-      "I'm sorry, I'm not programmed to handle offensive or inappropriate language. Please refrain from using such language in our conversation. 🚫",
-      "I'm here to assist you with any questions or problems you may have. How can I help you today? 🚀",
-      "Is there anything specific you'd like to talk about? 💬",
-      "I'm happy to help with any questions or concerns you may have. Just let me know how I can assist you. 😊",
-      "I'm here to assist you with any questions or problems you may have. What can I help you with today? 🤗",
-      "Is there anything specific you'd like to ask or talk about? I'm here to help with any questions or concerns you may have. 💬",
-      "I'm here to assist you with any questions or problems you may have. How can I help you today? 💡",
-    ];
-    
-    // Return a random response
-    return responses[Math.floor(Math.random() * responses.length)];
+async function generateResponse(input) {
+  try {
+    console.log(input)
+    const response = await fetch('https://api.openai.com/v1/chat/completions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer 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',
+        
+      },
+      body: JSON.stringify({
+        input:input[input.lenght-1].content,
+        prompt: input,
+        max_tokens: 50,
+        n: 1,
+        stop: ['\n']
+      })
+    });
+    const data = await response.json();
+    console.log(data); // log the response data to inspect its structure
+    const message = data.choices[0].text.trim();
+    return message;
+  } catch (error) {
+    console.error(error);
+    return "I'm sorry, I'm having trouble processing your request right now. Please try again later.";
   }
-  
+}
