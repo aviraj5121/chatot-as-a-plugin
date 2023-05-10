8cdc7fa19b455097fdd0f6d41dac5894a9a939ab
kartikgupta <kartik.gupta2505@gmail.com>
2023-05-10 19:07:32 +0530

    chatgpt api and with send button

--- Chatbot extensions/chatbot.js
+++ Chatbot extensions/chatbot.js
@@ -34,25 +34,23 @@ inputForm.addEventListener('submit', async function(event) {
 // Generate chatbot response function
 async function generateResponse(input) {
   try {
-    console.log(input)
     const response = await fetch('https://api.openai.com/v1/chat/completions', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer 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',
-        
+        'Authorization': 'Bearer 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'
       },
       body: JSON.stringify({
-        input:input[input.lenght-1].content,
-        prompt: input,
-        max_tokens: 50,
-        n: 1,
-        stop: ['\n']
+        model: "gpt-3.5-turbo",
+        messages: [{"role": "user", "content": input }],
+        "max_tokens": 50,
       })
-    });
+      
+    })
+    console.log(response)
     const data = await response.json();
-    console.log(data); // log the response data to inspect its structure
-    const message = data.choices[0].text.trim();
+    console.log(data.choices[0].message.content); // log the response data to inspect its structure
+    const message = data.choices[0].message.content
     return message;
   } catch (error) {
     console.error(error);
